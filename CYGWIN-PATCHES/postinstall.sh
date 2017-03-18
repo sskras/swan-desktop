5b8d4d5f8c20e19e302924604a1a4af7eaf42d65
Isaac W Hanson <isaac@starlig.ht>
2017-03-18 16:01:13 -0400

    start menu shortcut

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,2 +1,5 @@
 
 mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '-i /Swan.ico -w hide /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -D /bin/mintty
+
+mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '-i /Swan.ico -w hide /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/mintty
+
