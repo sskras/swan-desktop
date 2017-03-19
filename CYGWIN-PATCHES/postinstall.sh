ef327b383dd49e52517857498e305278cdcb97c1
Isaac W Hanson <isaac@starlig.ht>
2017-03-19 17:18:58 -0400

    fix shortcuts

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,5 +1,4 @@
 
-mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '-i /Swan.ico -w hide /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startxfce4"' -D /bin/mintty
-
-mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '-i /Swan.ico -w hide /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startxfce4"' -P /bin/mintty
+mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -D /bin/run
+mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/run
 
