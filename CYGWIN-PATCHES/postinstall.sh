fe371b66b29f92282ba63a8b8c2ab5fd14efd3b2
Isaac W Hanson <isaac@starlig.ht>
2017-03-26 16:39:52 -0400

    rm Cygwin-X shortcuts too

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -4,3 +4,4 @@ mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quot
 mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan/Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/run
 # remove previous versions' unintallable link
 rm -f "$(cygpath -P)/Swan Xfce4 Desktop.lnk"
+rm -rf "$(cygpath -P)/Cygwin-X"
