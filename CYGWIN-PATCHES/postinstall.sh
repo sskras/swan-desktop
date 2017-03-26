6fa25ed2feb8e7d30efd6358d613e1b2e9485ad2
Isaac W Hanson <isaac@starlig.ht>
2017-03-26 16:33:27 -0400

    cleanup old shtct

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,4 +1,6 @@
-
+# desktop icon
 mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -D /bin/run
+# start menu icon
 mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan/Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/run
-
+# remove previous versions' unintallable link
+rm -f "$(cygpath -P)/Swan Xfce4 Desktop.lnk"
