13cc34544e9b007123794233a4596d9e46b851ea
Isaac W Hanson <isaac@starlig.ht>
2017-03-26 14:42:50 -0400

    move shortcut

--- CYGWIN-PATCHES/postinstall.sh
+++ CYGWIN-PATCHES/postinstall.sh
@@ -1,4 +1,4 @@
 
 mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -D /bin/run
-mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/run
+mkshortcut.exe -d 'Swan Desktop' -i /Swan.ico -n 'Swan/Swan Xfce4 Desktop' -a '--quote /usr/bin/bash.exe -l -c "cd; exec /usr/bin/startx /etc/X11/xinit/Xsession xfce"' -P /bin/run
 
