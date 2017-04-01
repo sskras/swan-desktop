3cf2310101e57f2ef6d79f0df53ba11c3d7a7a2e
Isaac W Hanson <isaac@starlig.ht>
2017-04-01 17:32:18 -0400

    CHANGELOG

---
+++ usr/libexec/swan-desktop/swan-changelog.sh
@@ -0,0 +1,11 @@
+sleep 5
+timestamp="/var/run/swan-timestamp"
+changelog="/usr/share/doc/swan-desktop/CHANGELOG"
+text="See the changelog for important changes"
+if [ ! -e "$timestamp" -o "$timestamp" -ot "$changelog" ]; then
+    notify-send --icon dialog-warning "Important Changes!" "$text"
+    matedialog --notification --text "$text"
+    matedialog.exe --width=640 --height=480 --text-info --filename=/usr/share/doc/swan-desktop/CHANGELOG --title=CHANGELOG
+    touch $timestamp
+fi
+
